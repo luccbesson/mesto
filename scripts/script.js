ca3dd8d5f9fee359aa0b489b58c2aa56ba3c5afa
luccbesson <belkinalera@gmail.com>
2022-07-13 02:19:45 +0300

    feat: добавить функционал кнопок для попап

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+let name_origin = document.querySelector('.profile-info__name');
+document.getElementById('popup__profile-name').value = name_origin.innerHTML; //присваиваем форме оригинальные значения имени
+
+let description_origin = document.querySelector('.profile-info__description');
+document.getElementById('popup__profile-description').value = description_origin.innerHTML; //присваиваем форме оригинальные значения описания
+
+let formElement = document.querySelector('.popup__submit-button'); 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let nameInput = document.querySelector('.popup__profile-name').value;
+    let jobInput = document.querySelector('.popup__profile-description').value; 
+
+    // console.log(formElement);
+    // console.log(nameInput);
+    // console.log(jobInput);
+
+    name_origin.textContent = nameInput;
+    description_origin.textContent = jobInput;
+
+    // console.log(name_origin);
+    // console.log(description_origin);
+
+    document.getElementById('popup').style.display = "none";
+}
+formElement.addEventListener('click', formSubmitHandler);
+
+
+let closeElement = document.querySelector('.popup__close-icon');
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    document.getElementById('popup').style.display = "none";
+}
+closeElement.addEventListener('click', formSubmitHandler);
+
+
+
+
+
