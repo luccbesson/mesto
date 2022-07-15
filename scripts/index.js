f7c7f7db9afef35c6b33ba3b88e31109b426f45c
luccbesson <belkinalera@gmail.com>
2022-07-16 00:20:33 +0300

    fix: замечания по ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,37 @@
-let name_origin = document.querySelector('.profile-info__name');
-document.getElementById('popup__profile-name').value = name_origin.innerHTML; //присваиваем форме оригинальные значения имени
-
-let description_origin = document.querySelector('.profile-info__description');
-document.getElementById('popup__profile-description').value = description_origin.innerHTML; //присваиваем форме оригинальные значения описания
+let nameOrigin = document.querySelector('.profile-info__name');
+let descriptionOrigin = document.querySelector('.profile-info__description');
+let nameInput;
+let jobInput;
 
 let editElement = document.querySelector('.profile-info__edit-button');
-function editHandler(evt) {
-    evt.preventDefault();
+function editHandler() {
+    document.getElementById('popup__profile-name').value = nameOrigin.textContent; //присваиваем форме оригинальные значения имени
+    document.getElementById('popup__profile-description').value = descriptionOrigin.textContent; //присваиваем форме оригинальные значения описания
     //document.getElementById('overlay').style.display = "block";
     document.getElementById('overlay').classList.add('popup-overlay_opened');
-
 }
 editElement.addEventListener('click', editHandler);
 
-let formElement = document.querySelector('.popup__submit-button');
+//let formElement = document.querySelector('.popup__submit-button');
+let formElement = document.getElementById('submitform');
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__profile-name').value;
-    let jobInput = document.querySelector('.popup__profile-description').value;
+    nameInput = document.querySelector('.popup__profile-field_name').value;
+    jobInput = document.querySelector('.popup__profile-field_job').value;
     // console.log(formElement);
     // console.log(nameInput);
     // console.log(jobInput);
-    name_origin.textContent = nameInput;
-    description_origin.textContent = jobInput;
-    // console.log(name_origin);
-    // console.log(description_origin);
+    nameOrigin.textContent = nameInput;
+    descriptionOrigin.textContent = jobInput;
+    // console.log(nameOrigin);
+    // console.log(descriptionOrigin);
     //document.getElementById('overlay').style.display = "none";
     document.getElementById('overlay').classList.remove('popup-overlay_opened');
 }
-formElement.addEventListener('click', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
 
 let closeElement = document.querySelector('.popup__close-icon');
-function closeHandler(evt) {
-    evt.preventDefault();
-    //document.getElementById('overlay').style.display = "none";
+function closeHandler() {
     document.getElementById('overlay').classList.remove('popup-overlay_opened');
 }
 closeElement.addEventListener('click', closeHandler);
