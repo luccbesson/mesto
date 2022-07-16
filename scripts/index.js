fe4db450148c06b0196107a50b4cdbdd2755a513
luccbesson <belkinalera@gmail.com>
2022-07-16 22:54:00 +0300

    fix: замечания по ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,39 @@
-let nameOrigin = document.querySelector('.profile-info__name');
-let descriptionOrigin = document.querySelector('.profile-info__description');
-let nameInput;
-let jobInput;
+//присваиваем полю формы текущее (старое) значение поля профиля 
+let nameInput = document.querySelector('.profile-info__name');
+let jobInput = document.querySelector('.profile-info__description');
 
+//кнопка сохранения данных формы
+let formElement = document.getElementById('submitform');
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    // Получите значение полей jobInput и nameInput из свойства value
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textContent
+    nameInput.textContent = document.querySelector('.popup__profile-field_type_name').value; //изменить на id
+    jobInput.textContent = document.querySelector('.popup__profile-field_type_job').value; //изменить на id
+
+    closeHandler();
+}
+formElement.addEventListener('submit', formSubmitHandler); 
+
+//кнопка редактирования профиля
 let editElement = document.querySelector('.profile-info__edit-button');
 function editHandler() {
-    document.getElementById('popup__profile-name').value = nameOrigin.textContent; //присваиваем форме оригинальные значения имени
-    document.getElementById('popup__profile-description').value = descriptionOrigin.textContent; //присваиваем форме оригинальные значения описания
-    //document.getElementById('overlay').style.display = "block";
+    document.getElementById('popup__profile-name').value = nameInput.textContent;
+    document.getElementById('popup__profile-description').value = jobInput.textContent;
     document.getElementById('overlay').classList.add('popup-overlay_opened');
 }
 editElement.addEventListener('click', editHandler);
 
-//let formElement = document.querySelector('.popup__submit-button');
-let formElement = document.getElementById('submitform');
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    nameInput = document.querySelector('.popup__profile-field_name').value;
-    jobInput = document.querySelector('.popup__profile-field_job').value;
-    // console.log(formElement);
-    // console.log(nameInput);
-    // console.log(jobInput);
-    nameOrigin.textContent = nameInput;
-    descriptionOrigin.textContent = jobInput;
-    // console.log(nameOrigin);
-    // console.log(descriptionOrigin);
-    //document.getElementById('overlay').style.display = "none";
-    document.getElementById('overlay').classList.remove('popup-overlay_opened');
-}
-formElement.addEventListener('submit', formSubmitHandler);
-
+//кнопка закрытия формы
 let closeElement = document.querySelector('.popup__close-icon');
 function closeHandler() {
     document.getElementById('overlay').classList.remove('popup-overlay_opened');
 }
 closeElement.addEventListener('click', closeHandler);
+
+
+
+
+
+
