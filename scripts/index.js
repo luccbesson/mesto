0b8770b923f1992da84a2bb8e9f80627483f2b24
luccbesson <belkinalera@gmail.com>
2022-07-28 20:07:14 +0300

    Задача 1. Шесть карточек «из коробки»

--- scripts/index.js
+++ scripts/index.js
@@ -21,8 +21,8 @@ const initialCards = [
         link: './images/Алтай.jpg'
     },
     {
-        name: 'Карачаево-Черкесия',
-        link: './images/Карачаевск.jpg'
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     }
 ]
 //const cardsArray = Array.from(initialCards);
